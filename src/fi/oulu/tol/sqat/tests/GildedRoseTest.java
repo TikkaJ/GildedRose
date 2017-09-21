7f6c75e72a02794961fa96fb69ff90dbb7eb2687
jatikka <Adsam1990!>
2017-09-21 18:54:17 +0300

    Assignment 2 finished

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -34,9 +34,57 @@ public class GildedRoseTest {
 		Item itemBrie = items.get(0);
 		assertEquals(11, itemBrie.getQuality());
 	}
-    
 	@Test
-	public void testUpdateEndOfDay() {
-		fail("Test not implemented");
-	}
+	public void testUpdateEndOfDay_AgedBrie_sellin() {
+ 	
+ 		GildedRose store = new GildedRose();
+ 		store.addItem(new Item("Aged Brie", 2, 0) );
+ 		store.updateEndOfDay();
+ 		List<Item> items = store.getItems();
+ 		Item itemBrie = items.get(0);
+ 		assertEquals(1, itemBrie.getSellIn());
+ 	}
+ 	
+ 	@Test
+ 	public void testUpdateEndOfDay_mongoose_sellin() {
+ 	
+ 		GildedRose store = new GildedRose();
+ 		store.addItem(new Item("Elixir of the Mongoose", 5, 7) );
+ 	    store.updateEndOfDay();
+ 		List<Item> items = store.getItems();
+ 		Item mongooseElixir = items.get(0);
+ 		assertEquals(4, mongooseElixir.getSellIn());
+ 	}
+ 	
+ 	@Test
+ 	public void testUpdateEndOfDay_mongoose_quality() {
+ 	
+ 		GildedRose store = new GildedRose();
+ 		store.addItem(new Item("Elixir of the Mongoose", 5, 7) );
+ 		store.updateEndOfDay();
+ 		List<Item> items = store.getItems();
+ 		Item mongooseElixir = items.get(0);
+ 		assertEquals(6, mongooseElixir.getQuality());
+ 	}
+ 	
+ 	@Test
+ 	public void testUpdateEndOfDay_backstage_quality() {
+ 	
+ 		GildedRose store = new GildedRose();
+ 		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) );
+ 		store.updateEndOfDay();
+ 		List<Item> items = store.getItems();
+ 		Item backstage = items.get(0);
+ 		assertEquals(21, backstage.getQuality());
+ 	}
+ 	@Test
+ 	public void testUpdateEndOfDay_backstage_sellin() {
+ 		
+ 		GildedRose store = new GildedRose();
+ 		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) );
+ 		store.updateEndOfDay();
+ 		List<Item> items = store.getItems();
+ 		Item backstage = items.get(0);
+ 		assertEquals(14, backstage.getSellIn());
+ 	}
 }
